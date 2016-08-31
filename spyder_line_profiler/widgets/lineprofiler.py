01afd441a413f5f8147824d5909f38b76bcc0869
Joseph Martinot-Lagarde <joseph.martinot-lagarde@onera.fr>
2016-08-31 16:45:33 +0200

    QApplication is in QtWidgets now

--- spyder_line_profiler/widgets/lineprofiler.py
+++ spyder_line_profiler/widgets/lineprofiler.py
@@ -26,9 +26,9 @@ import sys
 from qtpy.compat import getopenfilename
 from qtpy.QtCore import (QByteArray, QProcess, Qt, QTextCodec,
                          QProcessEnvironment, Signal)
-from qtpy.QtGui import QApplication, QBrush, QColor, QFont
+from qtpy.QtGui import QBrush, QColor, QFont
 from qtpy.QtWidgets import (QHBoxLayout, QWidget, QMessageBox, QVBoxLayout,
-                            QLabel, QTreeWidget, QTreeWidgetItem)
+                            QLabel, QTreeWidget, QTreeWidgetItem, QApplication)
 
 # Local imports
 from spyder.config.base import get_conf_path, get_translation
