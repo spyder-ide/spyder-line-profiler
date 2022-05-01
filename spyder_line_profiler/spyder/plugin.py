9c1e538d5a19a8603f6aedd88e02742c19d57817
Simon Kern <14980558+skjerns@users.noreply.github.com>
2022-05-01 21:40:44 +0200

    add suggestions (pep8, rename function)
    Update spyder_line_profiler/spyder/confpage.py
    
    Co-authored-by: Carlos Cordoba <ccordoba12@gmail.com>
    
    Update spyder_line_profiler/spyder/plugin.py
    
    Co-authored-by: Carlos Cordoba <ccordoba12@gmail.com>
    
    remove api
    
    Update spyder_line_profiler/spyder/confpage.py
    
    Co-authored-by: Carlos Cordoba <ccordoba12@gmail.com>
    
    Update spyder_line_profiler/spyder/plugin.py
    
    Co-authored-by: Carlos Cordoba <ccordoba12@gmail.com>
    
    Update spyder_line_profiler/spyder/widgets.py
    
    Co-authored-by: Carlos Cordoba <ccordoba12@gmail.com>
    
    Update spyder_line_profiler/spyder/widgets.py
    
    Co-authored-by: Carlos Cordoba <ccordoba12@gmail.com>
    
    Update spyder_line_profiler/spyder/widgets.py
    
    Co-authored-by: Carlos Cordoba <ccordoba12@gmail.com>
    
    Update spyder_line_profiler/tests/test_lineprofiler.py
    
    Co-authored-by: Carlos Cordoba <ccordoba12@gmail.com>
    
    Update spyder_line_profiler/tests/test_lineprofiler.py
    
    Co-authored-by: Carlos Cordoba <ccordoba12@gmail.com>
    
    Update spyder_line_profiler/spyder/plugin.py
    
    Co-authored-by: Carlos Cordoba <ccordoba12@gmail.com>
    
    Update spyder_line_profiler/spyder/plugin.py
    
    Co-authored-by: Carlos Cordoba <ccordoba12@gmail.com>
    
    Update spyder_line_profiler/spyder/plugin.py
    
    Co-authored-by: Carlos Cordoba <ccordoba12@gmail.com>
    
    Update spyder_line_profiler/spyder/plugin.py
    
    Co-authored-by: Carlos Cordoba <ccordoba12@gmail.com>
    
    Update spyder_line_profiler/spyder/plugin.py
    
    Co-authored-by: Carlos Cordoba <ccordoba12@gmail.com>
    
    Update spyder_line_profiler/spyder/widgets.py
    
    Co-authored-by: Carlos Cordoba <ccordoba12@gmail.com>
    
    Update spyder_line_profiler/spyder/plugin.py
    
    Co-authored-by: Carlos Cordoba <ccordoba12@gmail.com>
    
    Update spyder_line_profiler/spyder/plugin.py
    
    Co-authored-by: Carlos Cordoba <ccordoba12@gmail.com>
    
    Update spyder_line_profiler/spyder/widgets.py
    
    Co-authored-by: Carlos Cordoba <ccordoba12@gmail.com>
    
    Update spyder_line_profiler/spyder/plugin.py
    
    Co-authored-by: Carlos Cordoba <ccordoba12@gmail.com>
    
    Update spyder_line_profiler/spyder/plugin.py
    
    Co-authored-by: Carlos Cordoba <ccordoba12@gmail.com>
    
    Update spyder_line_profiler/spyder/plugin.py
    
    Co-authored-by: Carlos Cordoba <ccordoba12@gmail.com>
    
    Update spyder_line_profiler/spyder/plugin.py
    
    Co-authored-by: Carlos Cordoba <ccordoba12@gmail.com>
    
    Update spyder_line_profiler/spyder/widgets.py
    
    Co-authored-by: Carlos Cordoba <ccordoba12@gmail.com>
    
    Update spyder_line_profiler/spyder/widgets.py
    
    Co-authored-by: Carlos Cordoba <ccordoba12@gmail.com>
    
    Update spyder_line_profiler/spyder/widgets.py
    
    Co-authored-by: Carlos Cordoba <ccordoba12@gmail.com>
    
    Update spyder_line_profiler/spyder/widgets.py
    
    Co-authored-by: Carlos Cordoba <ccordoba12@gmail.com>
    
    Update spyder_line_profiler/spyder/widgets.py
    
    Co-authored-by: Carlos Cordoba <ccordoba12@gmail.com>
    
    Update spyder_line_profiler/spyder/widgets.py
    
    Co-authored-by: Carlos Cordoba <ccordoba12@gmail.com>
    
    Update spyder_line_profiler/spyder/widgets.py
    
    Co-authored-by: Carlos Cordoba <ccordoba12@gmail.com>
    
    Update spyder_line_profiler/spyder/widgets.py
    
    Co-authored-by: Carlos Cordoba <ccordoba12@gmail.com>
    
    Update spyder_line_profiler/spyder/widgets.py
    
    Co-authored-by: Carlos Cordoba <ccordoba12@gmail.com>
    
    Update spyder_line_profiler/spyder/widgets.py
    
    Co-authored-by: Carlos Cordoba <ccordoba12@gmail.com>
    
    Update spyder_line_profiler/spyder/widgets.py
    
    Co-authored-by: Carlos Cordoba <ccordoba12@gmail.com>
    
    Apply suggestions from code review
    
    Co-authored-by: Daniel Althviz Moré <d.althviz10@uniandes.edu.co>
    
    remove pesky whitespaces
    
    Apply suggestions from code review
    
    Co-authored-by: Daniel Althviz Moré <d.althviz10@uniandes.edu.co>

--- spyder_line_profiler/spyder/plugin.py
+++ spyder_line_profiler/spyder/plugin.py
@@ -9,8 +9,11 @@
 Spyder Line Profiler 5 Plugin.
 """
 
+# Standard library imports
+import os.path as osp
+
 # Third-party imports
-from qtpy.QtCore import Signal
+from qtpy.QtCore import Qt, Signal
 from qtpy.QtGui import QIcon
 from spyder.api.plugins import Plugins, SpyderDockablePlugin
 from spyder.api.translations import get_translation
@@ -21,18 +24,16 @@ from spyder.plugins.mainmenu.api import ApplicationMenus
 # Local imports
 from spyder_line_profiler.spyder.confpage import SpyderLineProfilerConfigPage
 from spyder_line_profiler.spyder.widgets import SpyderLineProfilerWidget
-from spyder_line_profiler.spyder.widgets import SpyderLineProfilerWidgetActions
 from spyder_line_profiler.spyder.widgets import is_lineprofiler_installed
 
 # Localization
 _ = get_translation("spyder_line_profiler.spyder")
 
 
-
 class SpyderLineProfilerActions:
     # Triggers
     Run = 'run_profiler_action'
-    
+
 
 class SpyderLineProfiler(SpyderDockablePlugin):
     """
@@ -40,12 +41,13 @@ class SpyderLineProfiler(SpyderDockablePlugin):
     """
 
     NAME = "spyder_line_profiler"
-    REQUIRES = [Plugins.Editor]
+    REQUIRES = [Plugins.Preferences, Plugins.Editor]
     OPTIONAL = [Plugins.MainMenu]
     TABIFY = [Plugins.Help]
     WIDGET_CLASS = SpyderLineProfilerWidget
     CONF_SECTION = NAME
     CONF_WIDGET_CLASS = SpyderLineProfilerConfigPage
+    CONF_FILE = True
 
     # --- Signals
     sig_finished = Signal()
@@ -61,37 +63,46 @@ class SpyderLineProfiler(SpyderDockablePlugin):
         return _("Line profiler display for Spyder")
 
     def get_icon(self):
-        return QIcon('./data/images/spyder.line_profiler.png')
+        path = osp.join(
+            osp.dirname(osp.dirname(__file__)),
+            'data', 'images', 'spyder.line_profiler.png')
+        return QIcon(path)
 
     def on_initialize(self):
         self.widget = self.get_widget()
         self.widget.sig_finished.connect(self.sig_finished)
-        
+
         run_action = self.create_action(
             SpyderLineProfilerActions.Run,
             text=_("Run line profiler"),
             tip=_("Run line profiler"),
             icon=self.get_icon(),
             triggered=self.run_lineprofiler,
+            context=Qt.ApplicationShortcut,
             register_shortcut=True,
-        )   
+        )
         run_action.setEnabled(is_lineprofiler_installed())
-        
+
     @on_plugin_available(plugin=Plugins.MainMenu)
     def on_main_menu_available(self):
         mainmenu = self.get_plugin(Plugins.MainMenu)
         run_action = self.get_action(SpyderLineProfilerActions.Run)
         mainmenu.add_item_to_application_menu(
             run_action, menu_id=ApplicationMenus.Run)
-        
+
     @on_plugin_teardown(plugin=Plugins.MainMenu)
     def on_main_menu_teardown(self):
         mainmenu = self.get_plugin(Plugins.MainMenu)
         mainmenu.remove_item_from_application_menu(
-            SpyderLineProfilerActions.ProfileCurrentFile,
+            SpyderLineProfilerActions.Run,
             menu_id=ApplicationMenus.Run
-        )    
-        
+        )
+
+    @on_plugin_available(plugin=Plugins.Preferences)
+    def on_preferences_available(self):
+        preferences = self.get_plugin(Plugins.Preferences)
+        preferences.register_plugin_preferences(self)
+
     def check_compatibility(self):
         valid = True
         message = ""  # Note: Remember to use _("") to localize the string
@@ -99,7 +110,7 @@ class SpyderLineProfiler(SpyderDockablePlugin):
 
     def on_close(self, cancellable=True):
         return True
-        
+
     # --- Public API
     # ------------------------------------------------------------------------
     def update_pythonpath(self):
@@ -111,14 +122,14 @@ class SpyderLineProfiler(SpyderDockablePlugin):
         PYTHONPATH in Spyder.
         """
         self.widget.spyder_pythonpath = self.main.get_spyder_pythonpath()
-        
+
     def run_lineprofiler(self):
         """Run line profiler."""
         editor = self.get_plugin(Plugins.Editor)
         if editor.save():
             self.switch_to_plugin()
             self.analyze(editor.get_current_filename())
-        
+
         self.analyze(self.main.editor.get_current_filename())
 
     def analyze(self, filename):
